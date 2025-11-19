a503d57b4ed3a82b7666b4fd41a2399e0d6c6ae1
rene <prene@tlu.ee>
2025-11-19 11:27:24 +0200

    chore: change metadata and remove .env warning from landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Nutikas Ostukorv',
+  description: 'Poodide hindade võrdlemise rakendus',
 }
 
 const geistSans = Geist({
