8d3046d414bd44e0f1b29b6c5de1c2f68aeb85f0
KL1 <karl@luberg.ee>
2025-11-22 16:31:47 +0200

    refactor(categories): add pictures for categories on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -48,19 +48,48 @@ export default function Home() {
             <div className="w-full overflow-x-auto">
               <div className="flex gap-6 px-1 pb-3">
                 {[
-                  'Piimatooted',
-                  'Köögiviljad',
-                  'Liha',
-                  'Puuviljad',
-                  'Joogid',
-                  'Sügavkülmutatud',
-                  'Pagaritooted',
-                  'Maiustused',
-                ].map((name, i) => (
+                  {
+                    name: 'Piimatooted',
+                    image:
+                      'https://i0.wp.com/www.kabilaifarm.com/wp-content/uploads/2023/06/Understanding-the-Different-Types-of-Dairy-Products.jpg',
+                  },
+                  {
+                    name: 'Köögiviljad',
+                    image:
+                      'https://images.delfi.ee/media-api-image-cropper/v1/b8fb1020-b7e4-11eb-b469-85a469adc0e4.jpg?noup&w=1200&h=711&ch=0.8065&cw=1&cx=0&cy=0.0224&r=16:9',
+                  },
+                  {
+                    name: 'Liha',
+                    image:
+                      'https://www-assets.liebherr.com/media/bu-media/lhbu-hau/global/freshmag/meat/fleisch___biofleisch/is-meat-healthy-liebherr-freshmag-169-600.webp',
+                  },
+                  {
+                    name: 'Joogid',
+                    image: 'https://hshambaravi.ee/wp-content/uploads/2020/04/fresh-juice-2.jpg',
+                  },
+                  {
+                    name: 'Külmutatud',
+                    image:
+                      'https://cool-simple.com/cdn/shop/articles/88b703198fe3ad51308b02c2d9b2f7a6_6ffe6823-51df-4310-8ba5-89c25452ec02.jpg?v=1757623997&width=1200',
+                  },
+                  {
+                    name: 'Pagaritooted',
+                    image:
+                      'https://fitnessest.com/wp-content/uploads/2017/08/pexels-photo-209291-e1508062444626.jpeg',
+                  },
+                  {
+                    name: 'Maiustused',
+                    image:
+                      'https://www.sweets4me.co.uk/cdn/shop/collections/traditionalloose.jpg?v=1634895788',
+                  },
+                ].map((cat, i) => (
                   <Card key={i} className="min-w-[200px] cursor-pointer hover:shadow-lg transition">
-                    <div className="h-32 bg-muted-foreground/40 rounded-t-xl" />
+                    <div className="h-32 bg-muted-foreground/40 rounded-t-xl overflow-hidden">
+                      <img src={cat.image} alt={cat.name} className="w-full h-full object-cover" />
+                    </div>
+
                     <CardContent>
-                      <CardTitle>{name}</CardTitle>
+                      <CardTitle>{cat.name}</CardTitle>
                       <p className="text-sm text-muted-foreground mt-2">Kategooria kirjeldus</p>
                     </CardContent>
                   </Card>
