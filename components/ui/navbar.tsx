0f170fd7b7d7dc24582a693b8ec4021edc7caf6f
rene <prene@tlu.ee>
2025-12-03 17:38:48 +0200

    style: made navbar stick to top

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 
 export default function Navbar({ title = 'Nutikas ostukorv', right }: NavbarProps) {
   return (
-    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-20">
+    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-20 sticky top-0 z-50 bg-white/95 backdrop-blur-sm">
       <div className="w-full max-w-5xl flex items-center justify-between p-3 px-5 text-sm">
         <div className="flex gap-5 items-center font-semibold">
           <Link href="/" className="text-xl">
