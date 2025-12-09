4c1e5926ea0bd6c39a91af916c94cfe8b079295c
rene <prene@tlu.ee>
2025-12-09 12:51:21 +0200

    fix(category-client-wrapper): fix type error

--- components/category-client-wrapper.tsx
+++ components/category-client-wrapper.tsx
@@ -4,20 +4,14 @@ import { useState } from 'react'
 import GlobalProductSearch from '@/components/ui/global-product-search'
 import ProductInfoModal from '@/components/ui/product-info-modal'
 
-export default function CategoryClientWrapper({ allProducts }) {
+export default function CategoryClientWrapper({ allProducts }: { allProducts: any[] }) {
   const [selectedProduct, setSelectedProduct] = useState(null)
 
   return (
     <>
-      <GlobalProductSearch
-        allProducts={allProducts}
-        onSelectProduct={p => setSelectedProduct(p)}
-      />
+      <GlobalProductSearch allProducts={allProducts} onSelectProduct={p => setSelectedProduct(p)} />
 
-      <ProductInfoModal
-        product={selectedProduct}
-        onClose={() => setSelectedProduct(null)}
-      />
+      <ProductInfoModal product={selectedProduct} onClose={() => setSelectedProduct(null)} />
     </>
   )
 }
